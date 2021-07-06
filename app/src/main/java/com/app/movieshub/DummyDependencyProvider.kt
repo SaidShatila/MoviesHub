faf931ae3075055bd3d049ccb8ff83bed7d5def3
SaidShatila <57522137+SaidShatila@users.noreply.github.com>
2021-07-06 13:40:03 +0300

    Initialization of network and first Fragment implemantation and Repository

---
+++ app/src/main/java/com/app/movieshub/DummyDependencyProvider.kt
@@ -0,0 +1,27 @@
+package com.app.movieshub
+
+import com.app.movieshub.data.api.BASE_URL
+import com.app.movieshub.data.api.MovieRepository
+import com.app.movieshub.data.api.MovieService
+import com.app.movieshub.data.api.requestInterceptor
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+
+object DummyDependencyProvider {
+    val httpClient: OkHttpClient = OkHttpClient.Builder().addInterceptor(requestInterceptor).build()
+    val movieService: MovieService =
+        Retrofit.Builder().client(httpClient).baseUrl(BASE_URL)
+            .addConverterFactory(
+                Json {
+                    ignoreUnknownKeys = true
+                }.asConverterFactory("application/json".toMediaType())
+            )
+
+            .build()
+            .create(MovieService::class.java)
+    val movieRepository: MovieRepository = MovieRepository(movieService)
+}
